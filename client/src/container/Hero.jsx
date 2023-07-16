95cce4a6eeb42f4960222e060a9171cce2cf8903
Mahmud Mai <mahmudx00@gmail.com>
2023-07-16 22:23:52 +0100

    hero section

--- client/src/container/Hero.jsx
+++ client/src/container/Hero.jsx
@@ -1,16 +1,22 @@
 const Hero = () => {
   return (
     <div className="my-28">
+      <p className="breadcrumbs">
+        <span className="breadcrumbs-text">Intro</span>
+      </p>
+
       <p className="my-5 text-gray-400 text-xl lg:text-3xl">
         Available for hire/projects
       </p>
       {/* Some animation will be nice here*/}
-      <h1 className="my-8">Hey 👋, I am Mahmud. A fullstack Web developer</h1>
+      <h1 className="my-8">
+        Hey 👋, I am Mahmud Mai. A fullstack Web developer
+      </h1>
       <p className="my-5">
-        I am a full-stack developer with experience in the last three years. My
-        main expertise is Node js, React, Next Js, and Typescript. I am a
-        skilled and reliable freelancer looking for opportunities to build on
-        high-quality projects. I keep on challenging assignments,
+        I am a full-stack javascript developer with determination and passion
+        for learning. My area of expertise is React, Express, Node, and Mongo
+        DB. I am a skilled and reliable freelancer looking for opportunities to
+        build on high-quality projects. I keep on challenging assignments,
         problem-solving, and custom projects. I write clean, modular,
         well-documented code with security and scalability in mind and help
         create simple, friendly, accessible user interfaces.
