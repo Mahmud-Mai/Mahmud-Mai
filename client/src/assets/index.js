d7653f63234446d15f4b71689f64d9993a103de0
Mahmud Mai <mahmudx00@gmail.com>
2023-07-20 08:59:34 +0100

    add skill icons

---
+++ client/src/assets/index.js
@@ -0,0 +1,22 @@
+// Project Samples
+export { default as admin_dashboard } from "./project_images/admin_dashboard.png";
+export { default as cvas } from "./project_images/cvas.png";
+export { default as flightcompare } from "./project_images/flightcompare_hero-section.png";
+export { default as greenby } from "./project_images/greenby_hero-section.png";
+export { default as zelo } from "./project_images/zelo_hero-section.png";
+
+// Skills Icons
+export { default as chakraui } from "./skills_images/chakraui.png";
+export { default as tailwind } from "./skills_images/tailwind.png";
+export { default as express } from "./skills_images/express.png";
+export { default as mongodb } from "./skills_images/mongodb.png";
+export { default as wordpress } from "./skills_images/wordpress.png";
+export { default as css } from "./skills_images/css.png";
+export { default as git } from "./skills_images/git.png";
+export { default as html } from "./skills_images/html.png";
+export { default as javascript } from "./skills_images/javascript.png";
+export { default as mui } from "./skills_images/mui.png";
+export { default as node } from "./skills_images/node.png";
+export { default as react } from "./skills_images/react.png";
+export { default as sass } from "./skills_images/sass.png";
+export { default as redux } from "./skills_images/redux.png";
