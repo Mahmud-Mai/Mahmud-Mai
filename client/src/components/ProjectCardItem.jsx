c14f2cdbff69cf313ff89c89daf1f7e2da52c9bb
Mahmud Mai <mahmudx00@gmail.com>
2023-07-21 09:58:44 +0100

    create a reusable project card item

---
+++ client/src/components/ProjectCardItem.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+const ProjectCardItem = ({
+  imgSrc,
+  codeLink,
+  siteLink,
+  altText,
+  heading,
+  details,
+  techUsed,
+}) => {
+  return (
+    <div className="project_box">
+      <div>
+        <img
+          src={imgSrc}
+          alt={altText}
+        />
+        <h3>{heading}</h3>
+        <p>{details}</p>
+        <p className="my-2 italic text-gray-500">Tech used: {techUsed}</p>
+      </div>
+      <div className="project_box-links">
+        {codeLink !== "#" && <a href={codeLink}>Source code</a>}
+        <a href={siteLink}>Visit site</a>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCardItem;
