9dc74057671509d98ffdf71adffb9c05ecd67c91
Mahmud Mai <mahmudx00@gmail.com>
2023-07-26 01:53:57 +0100

    fix typo

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -11,7 +11,7 @@ const Experience = () => {
       <div className="mb-8">
         <Heading
           breadCrumbsText="Skills"
-          headingText="Technology I Use"
+          headingText="Technologies I Use"
         />
       </div>
 
